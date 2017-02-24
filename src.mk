ce8b2733951616c3e4e83cadb327bfae9102475c
Ian Wordes <iwordes@student.42.us.org>
2017-02-23 20:12:28 -0800

    State commit: src.mk

--- src.mk
+++ src.mk
@@ -1,16 +1,30 @@
 # Functions for 21sh
 
 SRC       = main.c init.c shell.c uninit.c
-SRC      += input.c
+SRC      += input.c lex.c parse.c
 
 SRC_BI    = echo.c exit.c
 
-SRC_IN    = up.c down.c right.c left.c
-SRC_IN   += home.c end.c
-SRC_IN   += bksp.c
-SRC_IN   +=
+SRC_CMD   = \
+init.c        parse.c       push_redir.c  split_args.c  split_pipe.c \
+input.c       push_argv.c   split.c       split_logic.c val.c
+
+SRC_CMDS  = grow.c init.c
+
+SRC_ENV   = del.c get.c get_safe.c grow.c list.c pwd.c set.c setkey.c
+
+SRC_EXEC  = bi.c cmd.c cmds.c name.c path.c wait.c
+
+#SRC_IN    = up.c down.c right.c left.c
+#SRC_IN   += home.c end.c
+#SRC_IN   += bksp.c
+#SRC_IN   +=
+
+SRC_LX    = pipe.c redir.c sep.c str.c str1_2.c
 
 SRC_KV    = cmp.c new.c val.c
+
+SRC_TK    = del.c new.c
 
 SRC_TM    = up.c down.c right.c left.c bksp.c
 SRC_TM   += clr_eol.c #clr_sol.c clr_ln.c
@@ -20,7 +34,7 @@ SRC_TM   += putchar.c putstr.c
 
 SRC_ALS   =
 
-SRC_CMD   = exec.c
+SRC_CMD   =
 
 SRC_ENV   = del.c get.c grow.c set.c setkey.c
 
@@ -38,11 +52,13 @@ SRC_UNIT  = env.c tty.c var.c hist.c
 
 SRC      += $(addprefix bi/,$(SRC_BI))
 SRC      += $(addprefix in/,$(SRC_IN))
+SRC      += $(addprefix lx/,$(SRC_LX))
 SRC      += $(addprefix kv/,$(SRC_KV))
 SRC      += $(addprefix tm/,$(SRC_TM))
 SRC      += $(addprefix cmd/,$(SRC_CMD))
 SRC      += $(addprefix env/,$(SRC_ENV))
 SRC      += $(addprefix var/,$(SRC_VAR))
+SRC      += $(addprefix exec/,$(SRC_EXEC))
 SRC      += $(addprefix hist/,$(SRC_HIST))
 SRC      += $(addprefix init/,$(SRC_INIT))
 SRC      += $(addprefix alias/,$(SRC_ALS))
