950e830ba3150e27101341c8a21bd249407243ee
Ian Wordes <iwordes@student.42.us.org>
2017-02-26 12:32:31 -0800

    Slightly adjust debug_tokens

--- src/debug/tokens.c
+++ src/debug/tokens.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/26 11:48:37 by iwordes           #+#    #+#             */
-/*   Updated: 2017/02/26 11:57:38 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/02/26 12:31:41 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	debug_tokens(t_token *tk)
 	while (tk != NULL)
 	{
 		ft_printf("{\n");
-		ft_printf("\t\"type\": %u\n", tk->type);
-		ft_printf("\t\"adj\":  %v\n", (tk->adj) ? "true" : "false");
+		ft_printf("    \"type\": %u\n", tk->type);
+		ft_printf("    \"adj\":  %v\n", (tk->adj) ? "true" : "false");
 		if (tk->type != TK_REDIR)
-			ft_printf("\t\"val\":  \"%s\"\n", tk->val);
+			ft_printf("    \"val\":  \"%s\"\n", tk->val);
 		ft_printf("}\n");
 		tk = tk->next;
 	}
