65cb19ca3ab3a2bd74ac6c992ae44ddbd62396fe
Ian Wordes <iwordes@student.42.us.org>
2017-03-13 23:23:16 -0700

    Add in_eoi()

--- src/in/eoi.c
+++ src/in/eoi.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/13 20:47:35 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/13 21:39:49 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/13 22:59:46 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,34 @@
 ** Navigate to the end of input.
 */
 
-#define LN (in->ln[y])
+#define LN (in->ln[i])
+
+int		sum_lines(t_in *in)
+{
+	int		i;
+	int		l;
+
+	i = 0;
+	while (i < in->l)
+	{
+		l += LN.ps_len + LN.ln_len;
+		i += 1;
+	}
+	return (l);
+}
 
 bool	in_eoi(t_in *in)
 {
-	int		x;
-	int		y;
+	int		dx;
+	int		dy;
 
-	y = 0;
-	x = 0;
+	dx = sum_lines(in);
+	dy = (dx / g_sh.cols) - g_sh.cy;
+	dx = (dx % g_sh.cols) - g_sh.cx;
 	in->y = in->l - 1;
 	in->x = in->ln[in->y].ln_len - 1;
-	while (y < in->l)
-	{
-		x += LN.ps_len + LN.ln_len;
-		y += 1;
-	}
-	y += x / g_sh.cols;
-	x %= g_sh.cols;
-	tm_gotor(y - g_sh.cx, x - g_sh.cy);
+	g_sh.cx += dx;
+	g_sh.cy += dy;
+	tm_gotor(dx, dy);
 	return (false);
 }
