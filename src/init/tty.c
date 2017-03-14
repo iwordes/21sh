90d9725269c3dd7acaad68dddd7a4c3e5e331a4c
Ian Wordes <iwordes@student.42.us.org>
2017-03-14 14:05:00 -0700

    Enable raw input mode

--- src/init/tty.c
+++ src/init/tty.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/02 18:19:37 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/13 19:30:10 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/14 14:03:23 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,9 @@ void	init_tty(void)
 							| ICRNL | IXON);
 	g_sh.tm_cfg.c_oflag = OPOST | ONLCR;
 	g_sh.tm_cfg.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN);
-	//g_sh.tm_cfg.c_cc[VTIME] = 0
-	//g_sh.tm_cfg.c_cc[VMIN] = 1
-//	if (tcsetattr(0, TCSANOW, &g_sh.tm_cfg) == -1)
-//		exit(1);
+	g_sh.tm_cfg.c_cc[VTIME] = 0;
+	g_sh.tm_cfg.c_cc[VMIN] = 1;
+	if (tcsetattr(0, TCSANOW, &g_sh.tm_cfg) == -1)
+		exit(1);
 	g_sh.cols = tm_cols();
 }
