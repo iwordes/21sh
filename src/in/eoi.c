a8430bd0d431d885a8e94b93ed27e09a5d210cc5
Ian Wordes <iwordes@student.42.us.org>
2017-03-13 21:29:55 -0700

    Add in_eoi()

---
+++ src/in/eoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   eoi.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/13 20:47:35 by iwordes           #+#    #+#             */
+/*   Updated: 2017/03/13 21:29:00 by iwordes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sh.h>
+
+/*
+** Navigate to the end of input.
+*/
+
+#define LN (in->ln[y])
+
+bool	in_eoi(t_in *in)
+{
+	int		x;
+	int		y;
+
+	y = 0;
+	x = 0;
+	while (y < in->l)
+	{
+		x += LN.ps_len + LN.ln_len;
+		y += 1;
+	}
+	y += x / g_sh.cols;
+	x %= g_sh.cols;
+	tm_gotor(y - g_sh.cx, x - g_sh.cy);
+}
