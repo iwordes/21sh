054032ab8dfc0878fb19900bbeb909d93935e036
Ian Wordes <iwordes@student.42.us.org>
2017-03-14 14:25:17 -0700

    Fix cursor movement issue (properly decrement ln_len)

--- src/in/del.c
+++ src/in/del.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/13 21:39:02 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/14 14:00:31 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/14 14:24:18 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 bool	in_del(t_in *in)
 {
 	ft_strcut(in->ln[in->y].ln, in->x, 1);
+	in->ln[in->y].ln_len -= 1;
 	in_print(in);
 	in_left(in);
 	return (false);
