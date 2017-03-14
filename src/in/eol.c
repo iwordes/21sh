448e3e460e0149690e7b72aef85cbc421a009160
Ian Wordes <iwordes@student.42.us.org>
2017-03-13 22:43:26 -0700

    Modify algorithm for determining sol/eol; add in_eol()

--- src/in/eol.c
+++ src/in/eol.c
@@ -1,31 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sol.c                                              :+:      :+:    :+:   */
+/*   eol.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/13 21:40:33 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/13 22:40:00 by iwordes          ###   ########.fr       */
+/*   Created: 2017/03/13 22:02:00 by iwordes           #+#    #+#             */
+/*   Updated: 2017/03/13 22:38:57 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sh.h>
 
-/*
-** Navigate to the start of the current line.
-*/
-
 #define LN (in->ln[in->y])
 
 #define BX ((LN.ps_len + in->x) % g_sh.cols)
 #define BY ((LN.ps_len + in->x) / g_sh.cols)
-#define EX ((LN.ps_len) % g_sh.cols)
-#define EY ((LN.ps_len) / g_sh.cols)
+#define EX ((LN.ps_len + LN.ln_len) % g_sh.cols)
+#define EY ((LN.ps_len + LN.ln_len) / g_sh.cols)
 
 // This probably turned rather redunant. Worth investigating.
 
-bool	in_sol(t_in *in)
+bool	in_eol(t_in *in)
 {
 	int		dx;
 	int		dy;
