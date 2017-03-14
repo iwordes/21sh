1ed8e372701714408d830d3ead67785292c58d78
Ian Wordes <iwordes@student.42.us.org>
2017-03-14 13:53:14 -0700

    Update input()

--- src/input.c
+++ src/input.c
@@ -6,124 +6,16 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/01 16:05:57 by iwordes           #+#    #+#             */
-/*   Updated: 2017/02/26 11:22:14 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/14 13:51:24 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sh.h>
 
 /*
-static char	*g_patt[] =
-{
-	"",
-
-	"\x1",
-	"\x2",
-	"\x3",
-	"\x4",
-
-	"\n",
-	"\r",
-	"\b",
-	"\x7f",
-	"\e[A",
-	"\e[B",
-	"\e[C",
-	"\e[D",
-	TM_HOME,
-	TM_END,
-	NULL
-};
-
-static void	(*g_fn[])(char**, size_t*, size_t*) =
-{
-	in_eot,
-
-	in_home,
-	in_left,
-	in_reset,
-	in_eot,
-
-	in_enter,
-	in_enter,
-	in_bksp,
-	in_bksp,
-	in_up,
-	in_down,
-	in_right,
-	in_left,
-	in_home,
-	in_end
-};
-
-static void	in_it(t_in *in, const char *prompt)
-{
-	in->q = 0;
-	in->x = 0;
-	in->y = 0;
-	in->mem = 4;
-	MGUARD(in->ln = ft_memalloc(sizeof(t_inln) * 5));
-	in->ps1 = env_get_safe("PS1");
-	in->ps2 = env_get_safe("PS2");
-	in->ps1_len = ft_strlen(in->ps1);
-	in->ps2_len = ft_strlen(in->ps2);
-	in->ret = false;
-	tm_cur_save();
-}
-
-static void	in_grow(t_in *in)
-{
-	char	*new_put;
-
-	if (in->i < in->m)
-		return ;
-	while (in->m <= in->i)
-		in->m *= 2;
-	MGUARD(new_put = ft_strnew(in->m));
-	ft_strcpy(new_put, in->put);
-	free(in->put);
-	in->put = new_put;
-}
-
-static void	in_sert(t_in *in, char *buff)
-{
-	in_grow(in);
-	ft_strins(in->put, buff, in->i);
-	in->x += ft_strlen(buff);
-}
-
-char	*input(void)
-{
-	char	buff[8];
-	t_in	in;
-	int		i;
-
-	in_it(&in);
-	while ((*(uint64_t*)buff = 0) || read(0, buff, 7) >= 0)
-	{
-		p = ~0;
-		while (g_patt[p += 1] != NULL)
-			if (ft_strequ(g_patt[p], buff))
-			{
-				g_fn[p](&in);
-				if (in.ret)
-					return (in_join(&in));
-				break ;
-			}
-		if (g_patt[p] != NULL || ft_strlen(buff) != 1)
-			continue ;
-		in_sert(&in, buff);
-		in_print(&in);
-	}
-	in_dest(&in);
-	return (NULL);
-}
-*/
-
-/*
 ** Cheap placeholder for the real input().
 */
-
+/*
 char	*input(void)
 {
 	char	*in;
@@ -134,4 +26,115 @@ char	*input(void)
 		return (NULL);
 	}
 	return (in);
+}
+*/
+
+char	*g_key[] =
+{
+	"",
+
+	"\n",
+	"\r",
+
+	"\b",
+	"\x7f",
+
+	//"\e[A",
+	//"\e[B",
+	"\e[C",
+	"\e[D",
+
+	//"\e[H",
+	//"\e[F",
+
+	NULL
+};
+
+bool	(*g_fn[])(t_in*) =
+{
+	in_eot,
+
+	in_return,
+	in_return,
+
+	in_del,
+	in_del,
+
+	//in_up,
+	//in_down,
+	in_right,
+	in_left,
+
+	//in_home,
+	//in_end
+};
+
+static void	init_(t_in *in)
+{
+	g_sh.cx = 0;
+	g_sh.cy = 0;
+	MGUARD(in->ln = MALT(t_inln, 1));
+	in->mem = 1;
+	in->l = 1;
+	in->x = 0;
+	in->y = 0;
+	in->q = NULL;
+	in->ps1 = env_get_safe("PS1");
+	in->ps2 = env_get_safe("PS2");
+	in->ps1_len = ft_strlen(in->ps1);
+	in->ps2_len = ft_strlen(in->ps2);
+	MGUARD(in->ln->ln = ft_strnew(128));
+	in->ln->ps_len = in->ps1_len;
+	in->ln->ps = in->ps1;
+	in->ln->ln_len = 0;
+	in->ln->mem = 128;
+}
+
+static char	*end_(t_in *in)
+{
+	char	*ln;
+	size_t	i;
+
+	i = 0;
+	MGUARD(ln = ft_strnew(0));
+	while (i < in->l)
+	{
+		MGUARD(ln = ft_strdjoin(ln, in->ln[i].ln));
+		free(in->ln[i].ps);
+		free(in->ln[i].ln);
+		free(in->ln + i);
+		i += 1;
+	}
+	in_eoi(in);
+	tm_nextln();
+	// TODO: Append (flat) line to history
+	return (ln);
+}
+
+// If issues occur with live-reading, invert the lookup table and pattern-search
+// each buffer.
+
+#define ZEROBUF (*(uint64_t*)buff = 0)
+
+char		*input(void)
+{
+	char	buff[8];
+	t_in	in;
+	size_t	k;
+
+	init_(&in);
+	while (ZEROBUF || read(0, buff, 7) >= 0)
+	{
+		k = ~0;
+		while (g_key[k += 1] != NULL)
+			if (ft_strequ(g_key[k], buff))
+			{
+				if (g_fn[k](&in))
+					return (end_(&in));
+				break;
+			}
+		if (g_key[k] == NULL && buff[1] == 0)
+			in_sert(&in, buff);
+	}
+	return (end_(&in));
 }
