e1147ee4b003d5af334ffba67c1c5b3c319c03f0
Ian Wordes <iwordes@student.42.us.org>
2017-03-13 19:15:29 -0700

    Update sig_winch() to make it recalc cursor location

--- src/sig/winch.c
+++ src/sig/winch.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/07 18:19:16 by iwordes           #+#    #+#             */
-/*   Updated: 2017/02/25 12:21:06 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/13 19:15:02 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void	sig_winch(int sig)
 {
+	int		i;
+
+	i = (g_sh.cy * g_sh.cols) + g_sh.cx;
 	g_sh.cols = tm_cols();
 	g_sh.rows = tm_rows();
-	//in_render();
+	g_sh.cy = i / g_sh.cols;
+	g_sh.cy = i % g_sh.cols;
+	in_print();
 	(void)sig;
 }
