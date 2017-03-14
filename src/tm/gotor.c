4a345245549e980cbf3541e1193589858c44afa3
Ian Wordes <iwordes@student.42.us.org>
2017-03-14 14:00:16 -0700

    Fix typo

--- src/tm/gotor.c
+++ src/tm/gotor.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 13:21:35 by iwordes           #+#    #+#             */
-/*   Updated: 2017/02/05 16:01:07 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/14 13:59:54 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ** Go to a coordinate relative to the current cursor position.
 */
 
-void	tm_rgoto(int x, int y)
+void	tm_gotor(int x, int y)
 {
 	if (x < 0)
 		while (x++)
