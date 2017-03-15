39864d6709bf1732f9adecf822d751e9f21930dd
Ian Wordes <iwordes@student.42.us.org>
2017-03-15 13:50:43 -0700

    Fix issue with backspacing in the prompt (losing track of cursor position/moving left an extra space at the far right of a line)

--- src/in/del.c
+++ src/in/del.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/13 21:39:02 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/14 14:24:18 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/15 13:49:44 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 
 bool	in_del(t_in *in)
 {
-	ft_strcut(in->ln[in->y].ln, in->x, 1);
+	if (in->x == 0)
+		return (false);
+	ft_strcut(in->ln[in->y].ln, in->x - 1, 1);
 	in->ln[in->y].ln_len -= 1;
 	in_print(in);
-	in_left(in);
+	if (in->x < in->ln[in->y].ln_len)
+		in_left(in);
+	else
+		in->x -= 1;
 	return (false);
 }
