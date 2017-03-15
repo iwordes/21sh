29489d1b12a31911bcd29009beaf1c5cf0fa2c71
Ian Wordes <iwordes@student.42.us.org>
2017-03-15 14:54:09 -0700

    Improve in_right() for compatibility with in_sert()

--- src/in/right.c
+++ src/in/right.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/13 18:33:12 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/14 14:20:54 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/15 14:47:19 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,15 @@ bool	in_right(t_in *in)
 {
 	if (in->x == in->ln[in->y].ln_len)
 		return (false);
-	if (g_sh.cx + 1 == g_sh.cols)
+	g_sh.cx += 1;
+	in->x += 1;
+	if (g_sh.cx != g_sh.cols)
+		tm_right();
+	else
 	{
-		tm_nextln();
+		write(1, "\r\n", 2);
 		g_sh.cy += 1;
 		g_sh.cx = 0;
 	}
-	else
-	{
-		tm_right();
-		g_sh.cx += 1;
-	}
-	in->x += 1;
 	return (false);
 }
