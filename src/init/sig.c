8e5f09814bc11c8c85a9b41e32ce1ea429ab8ddc
Ian Wordes <iwordes@student.42.us.org>
2017-03-20 19:09:38 -0700

    Update signal handlers to use generic end/ignore

--- src/init/sig.c
+++ src/init/sig.c
@@ -6,38 +6,43 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/07 17:58:39 by iwordes           #+#    #+#             */
-/*   Updated: 2017/02/25 11:35:33 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/20 19:07:50 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sh.h>
 
-static void	sig_1(void)
+#define IGN(S) (S == 16 || S == 19 || S == 20 || S == 23 || S == 28 || S == 29)
+
+static void	auto_end(void)
 {
-	// Signals 1-15
+	size_t	i;
+
+	i = 1;
+	while (i < 32)
+	{
+		if (!IGN(i) && i != SIGKILL && i != SIGSTOP)
+			signal(i, sig_end);
+		i += 1;
+	}
+}
+
+static void	auto_ign(void)
+{
+	size_t	i;
+
+	i = 1;
+	while (i < 32)
+	{
+		if (IGN(i) && i != SIGKILL && i != SIGSTOP)
+			signal(i, SIG_IGN);
+		i += 1;
+	}
 }
 
 void	init_sig(void)
 {
-	sig_1();
-	signal(SIGHUP, sig_hup);
-	signal(SIGINT, sig_int);
-	signal(SIGQUIT, sig_quit);
-	signal(SIGILL, sig_ill);
-	signal(SIGTRAP, sig_trap);
-	signal(SIGABRT, sig_abrt);
-	signal(SIGEMT, sig_emt);
-	signal(SIGFPE, sig_fpe);
-	signal(SIGBUS, sig_bus);
-	signal(SIGSEGV, sig_segv);
-	signal(SIGSYS, sig_sys);
-	signal(SIGPIPE, sig_pipe);
-	signal(SIGALRM, sig_alrm);
-	signal(SIGTERM, sig_term);
-	signal(SIGURG, sig_urg);
-	signal(SIGTSTP, sig_tstp);
-	signal(SIGCONT, sig_cont);
-	signal(SIGCHLD, sig_chld);
-	signal(SIGTTIN, sig_ttin);
-	signal(SIGTTOU, sig_ttou);
+	auto_end();
+	auto_ign();
+	signal(SIGWINCH, sig_winch);
 }
--- src/sig/winch.c
+++ src/sig/winch.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/07 18:19:16 by iwordes           #+#    #+#             */
-/*   Updated: 2017/03/14 13:52:20 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/03/20 14:52:49 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 void	sig_winch(int sig)
 {
-	int		i;
+	struct winsize	w;
+	int				i;
 
+	signal(SIGWINCH, SIG_IGN);
+	;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 	i = (g_sh.cy * g_sh.cols) + g_sh.cx;
-	g_sh.cols = tm_cols();
+	g_sh.cols = w.ws_col;
 	g_sh.cy = i / g_sh.cols;
 	g_sh.cx = i % g_sh.cols;
+	;
+	signal(SIGWINCH, sig_winch);
 	(void)sig;
 }
