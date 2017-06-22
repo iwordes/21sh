eab847c59a5c6a20cf4925fc71f44cdf991fb540
Ian Wordes <iwordes@student.42.us.org>
2017-06-22 10:03:03 -0700

    Remove comment

--- src/input.c
+++ src/input.c
@@ -6,21 +6,11 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 14:28:23 by iwordes           #+#    #+#             */
-/*   Updated: 2017/06/13 19:45:46 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/06/22 10:02:47 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <main.h>
-
-/*
-** Goals:
-** 1. Allow intuitive line editing.
-** 2. Allow editing of multiple lines.
-**
-** Additionally:
-** 1. Manage input history.
-** 2. Manage a clipboard, because we certainly don't live in a graphical age.
-*/
 
 static t_inkey	g_key[] =
 {
